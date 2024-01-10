98af8c0881b218c4217db5844bc82c63c3ebc397
AntonioFernandez123 <151384441+AntonioFernandez123@users.noreply.github.com>
2024-01-10 17:12:14 +0100

    1

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+//Font Awesome
+import "../../node_modules/@fortawesome/fontawesome-free/js/all";
+
+//Simple Slider
+import { getSlider } from "simple-slider";
+
+getSlider({
+    container: document.getElementById("myslider"),
+    transitionTime:1,
+    delay:3.5
+});
+
+
